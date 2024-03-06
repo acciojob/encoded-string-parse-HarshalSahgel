163863c6489fe24861cc7387e2da683a9ffddb3d
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-03-06 21:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const parseCode = (str) => {
-  // your code here
+  const [firstName, lastName, id] = str.split("0").filter(Boolean);
+  return { firstName, lastName, id };
 };
 
 // Do not change the code below
